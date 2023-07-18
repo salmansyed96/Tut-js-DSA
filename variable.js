08d3cdf04ee23864a21e2f4720ad0b8ea4fde7a9
salmansyed96 <124539970+salmansyed96@users.noreply.github.com>
2023-07-18 20:19:00 +0530

    Add First-3

---
+++ variable.js
@@ -0,0 +1,48 @@
+// var digit = 12;
+// var y = 8;
+//  var x = digit + y;
+
+//  console.log
+// (x)
+
+
+
+// let y = 5, x = "pp";
+
+// let x ;
+// x = 10;
+// // let y = 3;
+//  x += 5
+
+//power
+//  console.log(x)
+
+// let x ;
+// x =5;
+// y = x ** 4;
+
+// console.log(y)
+
+
+// let x =5; //number
+// let y = "5"; //string
+// x = y;
+// console.log(x+y)
+
+// let x = "hello";
+// let y = "world";
+// console.log(x + " " + y );
+
+//add number vlue
+
+// let x = 5;
+// x++
+
+// x--
+// x--
+// //x =  x+5;
+// console.log(x)
+
+let x =5;
+let z =(x + 5) /5;
+console.log(z)
