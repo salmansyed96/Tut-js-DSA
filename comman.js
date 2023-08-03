c5bfa47bb5e5d03326ee2fb58efb80cf0c10acc1
salmansyed96 <124539970+salmansyed96@users.noreply.github.com>
2023-08-03 16:23:51 +0530

    nasted-loop practice

--- comman.js
+++ comman.js
@@ -15,10 +15,32 @@
 
 
 
-// for(let counter = 1; counter<= 10; counter++){
-//     document.write("Test2");
+// for(let counter = 1 ; counter<= 10; counter++){
+//     if(counter==5){
+//         //break;
+//         continue;
+//     }
+//     document.write(counter);
+//     document.write('<br>')
     
 // }
+
+outer:for(let counter = 1 ; counter<= 10; counter++){
+    
+    document.write(counter);
+    document.write('<br>')
+
+    for(let counter2 =1; counter2 <3; counter2++){
+
+        if(counter ==3 ){
+            break outer;
+        }
+
+        document.write('tech');
+        document.write('<br>');
+    }
+    
+}
 
 //while(counter<= 10)
 
---
+++ nested loop.js
@@ -0,0 +1,30 @@
+// for(let counter = 1 ; counter<= 10; counter++){
+    
+//     document.write(counter);
+//     document.write('<br>')
+
+//     for(let counter2 =1; counter2 <3; counter2++){
+//         document.write('tech');
+//         document.write('<br>');
+//     }
+    
+// }
+
+
+
+// outer:for(let counter = 1 ; counter<= 10; counter++){
+    
+//     document.write(counter);
+//     document.write('<br>')
+
+//     for(let counter2 =1; counter2 <3; counter2++){
+
+//         if(counter ==3 ){
+//             break outer;
+//         }
+
+//         document.write('tech');
+//         document.write('<br>');
+//     }
+    
+// }
